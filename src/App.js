c7eb5a20cf5b3c5dc6fe6ae9f9dc3ad6e4e716c1
marvin evins <maevins@cps.edu>
2018-11-22 08:39:23 -0600

    friends list react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = () => <PersonList />;
+
+const Person = props => {
+  const {img, name,job} = props.person;
+  const {children} = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+
+  return (
+    <div className="person">
+      <img src={url} alt="" />
+
+      <div>
+        <h4>{name}</h4>
+        <h4>{job}</h4>
+        {children}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+
+const PersonList = () => {
+  const people = [
+    {
+      img: 22,
+      name: "jon",
+      job: "developer"
+    },
+    {
+      img: 34,
+      name: "mabb",
+      job: "developer"
+    },
+    {
+      img: 45,
+      name: "dan",
+      job: "developer"
+    }
+  ];
+  return (
+    <section>
+      <Person person={people[0]}/>;
+      <Person person={people[1]}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+      </Person>;
+      <Person person={people[2]}/>;
+    </section>
+  );
+};
+
+
+
 
 export default App;
